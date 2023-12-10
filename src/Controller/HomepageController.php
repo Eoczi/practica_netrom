eed1c0c12e1b3663fc604599a072af1a5ec1e79b
Eoczi <koczi.eduard2311@gmail.com>
2023-12-10 16:15:36 +0200

    ranking table to pdf file functionality

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Exception;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,17 +17,4 @@ class HomepageController extends AbstractController
         ]);
     }
 
-    /**
-     * @throws Exception
-     */
-    #[Route('/clear', name: 'app_clear_database')]
-    public function clearDatabase(EntityManagerInterface $entityManager): Response
-    {
-        $connection = $entityManager->getConnection();
-        $platform = $connection->getDatabasePlatform();
-
-        $connection->executeUpdate($platform->getTruncateTableSQL('your_table_name', true));
-
-        return $this->redirectToRoute('');
-    }
 }
