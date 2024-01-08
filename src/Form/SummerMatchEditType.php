204152b44d538501685d34bb26c9c312e0cb4bbc
Eoczi <koczi.eduard2311@gmail.com>
2024-01-08 02:15:31 +0200

    winner logic fixed

--- src/Form/SummerMatchEditType.php
+++ src/Form/SummerMatchEditType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\SummerMatch;
 use App\Entity\Team;
+use App\Entity\TeamsHaveMatches;
+use App\Repository\TeamRepository;
+use App\Repository\TeamsHaveMatchesRepository;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -13,17 +16,21 @@ class SummerMatchEditType extends SummerMatchType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         //parent::buildForm($builder, $options);
+        $data = $builder->getData();
+        if ($data instanceof SummerMatch) {
+            $matchID = $data->getId();
+        }
 
         $builder->add('winner', EntityType::class, [
             'class' => Team::class,
-            'choice_label' => 'name',
-            /*'query_builder' => function (EntityRepository $er) {
+            'query_builder' => function (TeamRepository $er) use ($matchID) {
                 return $er->createQueryBuilder('t')
-                    ->select('thm.teamsHaveMatches as matches')
-                    ->from('App\Entity\TeamsHaveMatches', 'thm')
-                    ->where('thm.teamsHaveMatches = :teamID')
-                    ->setParameter('teamID', $teamID)
-            },*/
+                    ->join(TeamsHaveMatches::class, 'thm', 'WITH', 't.id = thm.teamsHaveMatches')
+                    ->join(SummerMatch::class, 'sm', 'WITH', 'sm.id = thm.matchesHaveTeams')
+                    ->where('thm.matchesHaveTeams = :matchID')
+                    ->setParameter('matchID', $matchID);
+            },
+            'choice_label' => 'name',
         ]);
     }
 
